62a55add5a2bddcbf81df8a380ef4e3d9a1ad8b8
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-03-19 18:04:34 +1300

    remove unused things

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Typography, Container } from '@mui/material';
+import { Container } from '@mui/material';
 import TopLeftLabel from './components/TopLeftLabel';
 import TorrentDropper from './components/TorrentDropper';
 import TorrentTable from './components/TorrentTable';
